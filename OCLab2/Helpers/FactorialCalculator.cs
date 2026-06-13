3521db359b5f0c76e8c30853e3edb90e9828a5b3
Вадим <@DESKTOP-AKVAKEN>
2026-06-13 21:15:08 +0300

    Добавьте файлы проекта.

---
+++ OCLab2/Helpers/FactorialCalculator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Numerics;
+
+namespace OCLab2.Helpers;
+
+public static class FactorialCalculator
+{
+    public static BigInteger Calculate(int n)
+    {
+        if (n < 0) return -1;
+        if (n == 0 || n == 1) return 1;
+
+        BigInteger result = 1;
+        for (int i = 2; i <= n; i++)
+        {
+            result *= i;
+        }
+        return result;
+    }
+
+    public static Dictionary<int, BigInteger> CalculateParallel(List<int> numbers)
+    {
+        var results = new ConcurrentDictionary<int, BigInteger>();
+
+        Parallel.ForEach(numbers, number =>
+        {
+            var factorial = Calculate(number);
+            results.TryAdd(number, factorial);
+        });
+
+        return results.ToDictionary(kv => kv.Key, kv => kv.Value);
+    }
+}
---
+++ OCLab2/ViewModels/MainViewModel.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Microsoft.Win32;
+using OCLab2.Helpers;
+
+namespace OCLab2.ViewModels;
+
+public class MainViewModel : INotifyPropertyChanged
+{
+    private List<int> _numbers = new();
+    public List<int> Numbers
+    {
+        get => _numbers;
+        set
+        {
+            _numbers = value;
+            OnPropertyChanged();
+        }
+    }
+
+    private Dictionary<int, BigInteger> _results = new();
+    public Dictionary<int, BigInteger> Results
+    {
+        get => _results;
+        set
+        {
+            _results = value;
+            OnPropertyChanged();
+        }
+    }
+
+    private string _status = "Выберите файл с числами";
+    public string Status
+    {
+        get => _status;
+        set
+        {
+            _status = value;
+            OnPropertyChanged();
+        }
+    }
+
+    private string _filePath = string.Empty;
+    public string FilePath
+    {
+        get => _filePath;
+        set
+        {
+            _filePath = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public ICommand LoadFileCommand { get; }
+    public ICommand CalculateCommand { get; }
+
+    public MainViewModel()
+    {
+        LoadFileCommand = new RelayCommand(_ => LoadFile());
+        CalculateCommand = new RelayCommand(_ => CalculateFactorials(), _ => Numbers.Count > 0);
+    }
+
+    private void LoadFile()
+    {
+        var dialog = new OpenFileDialog
+        {
+            Filter = "Текстовые файлы (*.txt)|*.txt|Все файлы (*.*)|*.*"
+        };
+
+        if (dialog.ShowDialog() == true)
+        {
+            FilePath = dialog.FileName;
+            var lines = File.ReadAllLines(FilePath);
+
+            Numbers = lines
+                .SelectMany(line => line.Split(new[] { ' ', '\t', ',' }, StringSplitOptions.RemoveEmptyEntries))
+                .Select(s => int.TryParse(s, out int n) ? n : (int?)null)
+                .Where(n => n.HasValue)
+                .Select(n => n!.Value)
+                .Where(n => n >= 0) 
+                .ToList();
+
+            Status = $"Загружено чисел: {Numbers.Count}";
+            Results = new Dictionary<int, BigInteger>();
+        }
+    }
+
+    private void CalculateFactorials()
+    {
+        Status = "Вычисление факториалов в параллельном режиме...";
+
+        Task.Run(() =>
+        {
+            var results = FactorialCalculator.CalculateParallel(Numbers);
+
+            var sorted = results
+                .OrderBy(kv => kv.Key)
+                .ToDictionary(kv => kv.Key, kv => kv.Value);
+
+            App.Current.Dispatcher.Invoke(() =>
+            {
+                Results = sorted;
+                Status = $"Вычислено факториалов: {Results.Count} (потоков: {Environment.ProcessorCount})";
+            });
+        });
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
+
+public class RelayCommand : ICommand
+{
+    private readonly Action<object?> _execute;
+    private readonly Predicate<object?>? _canExecute;
+
+    public RelayCommand(Action<object?> execute, Predicate<object?>? canExecute = null)
+    {
+        _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+        _canExecute = canExecute;
+    }
+
+    public bool CanExecute(object? parameter) => _canExecute?.Invoke(parameter) ?? true;
+    public void Execute(object? parameter) => _execute(parameter);
+
+    public event EventHandler? CanExecuteChanged
+    {
+        add => CommandManager.RequerySuggested += value;
+        remove => CommandManager.RequerySuggested -= value;
+    }
+}
